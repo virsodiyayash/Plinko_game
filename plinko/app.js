dd7dba33edae3363fc48ecd1da7eb2d658d528b4
Yash Virsodiya <virsodiyayash4@gmail.com>
2024-08-06 01:09:52 +0530

    app.js

--- plinko/app.js
+++ plinko/app.js
@@ -1,17 +1,24 @@
-function setup(){
-    createCanvas(600 , 600);
+function setup() {
+    let canvas = createCanvas(1150, 550);
+    canvas.parent('gameCanvas1'); 
+    
     fill(0);
     strokeWeight(3);
     rectMode(CENTER);
-    
+
     let space = 60;
-    
-    for(let x = 0 ; x < width + 50 ; x+=space){
-        for(let y = 0 ; y < height + 50 ; y+=space){
-            // line(x , y , x + space , y);
-            // line(x , y , x , y + space);
-            circle(x , y , 10);
-            circle(x + space / 2 , y + space / 2 , 10);
+
+    for (let x = 0; x < width; x += space) {
+        for (let y = 0; y < height; y += space) {
+           
+            if (y < space / 2) continue;
+
+            if (x < space / 2 || x > width - space / 2) continue;
+
+            if (y > height - space / 2) continue;
+
+            circle(x, y, 10);
+            circle(x + space / 2, y + space / 2, 10);
         }
     }
 }
